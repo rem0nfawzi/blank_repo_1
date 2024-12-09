a11eb3e637b11b99b0e42fb9e681573a1e3877a4
Remon Fawzi <remonfawzi@Remons-MacBook-Air.local>
2024-12-09 12:22:04 +0200

    _

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-console.log("ktdol")
-console.log("oqllr")
-console.log("atcep")
-console.log("xqbhh")
-console.log("hgxps")
+console.log("ktdol");
+console.log("oqllr");
+console.log("atcep");
+console.log("xqbhh");
+console.log("hgxps");
+console.log("__");
